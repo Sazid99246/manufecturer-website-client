ea1ccacf4a4842431d9a4590c0f005296b009741
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-05-22 11:50:05 +0600

    designed login page and shown error with react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+    };
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='max-h-screen flex justify-center items-center' onSubmit={handleSubmit(onSubmit)}>
+            <div class="card w-96 shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <form action="">
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input
+                                {...register("email",
+                                    {
+                                        required: true,
+                                        pattern: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+                                    }
+                                )}
+                                type="email"
+                                placeholder="Enter your email"
+                                class="input input-bordered"
+                            />
+                            <label class="label">
+                                <span className='label-text-alt text-red-500'>
+                                    {errors.email?.type === 'required' && "Email is required"}
+                                    {errors.email?.type === 'pattern' && "Enter a valid email"}
+                                </span>
+                            </label>
+
+                        </div>
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Password</span>
+                            </label>
+                            <input
+                                {...register("password",
+                                    {
+                                        required: true,
+                                        minLength: 6
+                                    }
+                                )}
+                                type="password"
+                                placeholder="Enter your password"
+                                class="input input-bordered"
+                            />
+                            <label class="label">
+                                <span className='label-text-alt text-red-500'>
+                                    {errors.password?.type === 'required' && "Password is required"}
+                                    {errors.password?.type === 'minLength' && "Password must be at least 6 characters or longer"}
+                                </span>
+                            </label>
+                            <label class="label">
+                                <a href="/" class="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div class="form-control mt-6">
+                            <button class="btn btn-primary">Login</button>
+                        </div>
+                        <div class="form-control mt-6">
+                            <p>Don't have an account?
+                                <Link to='/signup' class="btn btn-link">Register now</Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
