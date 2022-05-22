a28127fc30eb28e8f4cbf758ef12f61ba0d1d0e8
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-05-22 07:13:48 +0600

    added simple header and footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Purchase from './Pages/Purchase/Purchase';
 import SignUp from './Pages/SignUp/SignUp';
+import Footer from './Shared/Footer/Footer';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/purchase' element={<Purchase/>}/>
-        <Route path='/blogs' element={<Blogs/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/purchase' element={<Purchase />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
