a39c247cfa06b43ca214f188e4332d9f27bee13b
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-05-25 11:49:47 +0600

    added dashboaed and nested 3 routes within it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import NotFound from './NotFound/NotFound';
 import Blogs from './Pages/Blogs/Blogs';
 import Dashboard from './Pages/DashBoard/Dashboard';
+import MyOrders from './Pages/DashBoard/MyOrders';
+import MyProfile from './Pages/DashBoard/MyProfile';
+import MyReview from './Pages/DashBoard/MyReview';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Purchase from './Pages/Purchase/Purchase';
@@ -31,11 +34,23 @@ function App() {
               <Purchase />
             </RequireAuth>
           } />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route
+          path='/dashboard'
+          element={
+            <RequireAuth>
+              <Dashboard>
+              </Dashboard>
+            </RequireAuth>
+          }
+        >
+          <Route index element={<MyOrders />}></Route>
+          <Route path='myreview' element={<MyReview />}></Route>
+          <Route path='myportfolio' element={<MyProfile />}></Route>
+        </Route>
         <Route path='/blogs' element={<Blogs />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='*' element={<NotFound />} />  
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
